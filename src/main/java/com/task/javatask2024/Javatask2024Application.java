eb0742835c555d96c7c04f530c28eca6e8d579a5
shako <S.DAVITASHVILI@OPTIMOGROUP.IO>
2024-07-30 19:21:07 +0400

    some changes

---
+++ src/main/java/com/task/javatask2024/Javatask2024Application.java
@@ -0,0 +1,13 @@
+package com.task.javatask2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javatask2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javatask2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/javatask2024/Javatask2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.javatask2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javatask2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
